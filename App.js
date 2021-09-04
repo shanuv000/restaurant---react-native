0775d7945cd41eaefc4ad802560a367a04d337f1
VAIBHAV KUMAR <48181718+shanuv000@users.noreply.github.com>
2021-09-04 19:51:13 +0530

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+import React from "react";
+// import { createStackNavigator, createAppContainer } from "react-navigation";
+import SearchScreens from "./src/screens/SearchScreens";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+const navigator = createStackNavigator(
+  {
+    Search: SearchScreens,
   },
-});
+  {
+    initialRouteName: "Search",
+    defaultNavigationOptions: { title: "Business" },
+  }
+);
+export default createAppContainer(navigator);
